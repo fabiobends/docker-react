d95bead7a20d822f0a4cde6071d3d51864321505
Fábio Mendes <fabio.mendes@yellowipe.io>
2023-12-15 21:51:30 -0300

    feat: update text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey</p>
+        <p>Hey, something new over here!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
